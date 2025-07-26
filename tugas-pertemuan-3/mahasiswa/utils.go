9072ea754de112da6a802ab7d103fd6c0b1a5a83
unknown <luthfirobbaniy@gmail.com>
2025-07-26 13:39:37 +0700

    refactor tugas pertemuan 3 - memindahkan fungsi untuk menjumlahkan umur ke utils.go

--- tugas-pertemuan-3/mahasiswa/utils.go
+++ tugas-pertemuan-3/mahasiswa/utils.go
@@ -1,5 +1,9 @@
 package mahasiswa
 
+type Ages struct {
+	Total int
+}
+
 var maxNilai int = 100
 var Versi string = "v1.0.0"
 
@@ -24,4 +28,17 @@ func BuatMahasiswa(nama string, umur int, nilai ...int) *Mahasiswa {
 
 func GetMaxNilai() int {
 	return maxNilai
+}
+
+func (a *Ages) Sum() func(ages ...int) *Ages {
+	return func(ages ...int) *Ages {
+		for _, age := range ages {
+			a.Total += age
+		}
+		return a
+	}
+}
+
+func (a *Ages) GetTotal() int {
+	return a.Total
 }
