de137c3219f126a764ed3eba258a8883aee6ce9e
unknown <luthfirobbaniy@gmail.com>
2025-08-16 07:39:43 +0700

    refctor: tugas pertemuan 6 - Student model swagger example tag

--- tugas-pertemuan-6/models/student.go
+++ tugas-pertemuan-6/models/student.go
@@ -1,10 +1,10 @@
 package models
 
 type Student struct {
-	ID       int    `json:"id"`
-	NIM      string `json:"nim" example:"2021003"`
-	Name     string `json:"name" example:"Budi"`
-	Email    string `json:"email" example:"budi@example.com"`
-	Major    string `json:"major" example:"Teknik Industri"`
-	Semester int    `json:"semester" example:"2"`
+	ID       int    `json:"id" example:"1"`
+	NIM      string `json:"nim" example:"2021001"`
+	Name     string `json:"name" example:"Luthfi"`
+	Email    string `json:"email" example:"luthfi@example.com"`
+	Major    string `json:"major" example:"Computer Science"`
+	Semester int    `json:"semester" example:"1"`
 }
